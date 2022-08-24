da56a283343a4abe8e3855b87e9eb3e908b8b38f
oswaldochuquipoma <42358018+oswaldochuquipoma@users.noreply.github.com>
2022-08-24 10:11:25 -0500

    establecimietno

---
+++ models/establecimiento.model.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, type) => {
+    var Establecimiento = sequelize.define(
+      "maestro_his_establecimiento",
+      {
+        id: {
+          type: type.INTEGER,
+          primaryKey: true,
+          field: "id_establecimiento",
+        },
+        establecimiento: { type: type.STRING, field: "nombre_establecimiento" },
+        codigo: { type: type.STRING, field: "codigo_unico" },        
+      },
+      {
+        timestamps: false,
+        freezeTableName: true,
+      }
+    );
+    Establecimiento.associate = function (models) {
+        Establecimiento.hasOne(models.his_hoja_atencion, {
+        foreignKey: {
+          name: "codigo_unico_ipress",
+        },
+      });
+      Establecimiento.hasOne(models.personal, {
+        foreignKey: {
+          name: "ID_ESTABLECIMIENTO",
+        },
+      });
+    };
+    return Establecimiento;
+  };
