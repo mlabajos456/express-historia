7456cb033d477f80fedb1d43f8885cd234c4cd53
oswaldochuquipoma <42358018+oswaldochuquipoma@users.noreply.github.com>
2022-08-24 09:12:22 -0500

    oswaldoido

--- models/paciente.model.js
+++ models/paciente.model.js
@@ -20,6 +20,43 @@ module.exports = (sequelize, type) => {
           return moment(this.getDataValue("f_nacimiento")).format("DD-MM-YYYY");
         },
       },
+      tipodoc: {
+        type: type.STRING,
+        field: "id_tipo_documento",
+      },
+      documento: {
+        type: type.STRING,
+      },
+      nombres: {
+        type: type.STRING,
+      },
+      ape_paterno: {
+        type: type.STRING,
+      },
+      ape_materno: {
+        type: type.STRING,
+      },
+      sexo: {
+        type: type.STRING,
+      },
+      ubigeo: {
+        type: type.STRING,
+        field: "id_ubigeo",
+      },
+      ubigeo: {
+        type: type.STRING,
+        field: "id_ubigeo",
+      },
+      codigo: {
+        type: type.STRING,
+        field: "cod_paciente",
+      },
+      telefono: {
+        type: type.STRING,
+      },
+      direccion: {
+        type: type.STRING,
+      },
     },
     {
       timestamps: false,
