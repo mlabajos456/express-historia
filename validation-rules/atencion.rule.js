bdaaa35e0ca5fbe8dbc402610174ef4b2369e8a2
mlabajos456 <tigerleep26@gmail.com>
2022-08-25 12:43:43 -0500

    Update atencion.rule.js

--- validation-rules/atencion.rule.js
+++ validation-rules/atencion.rule.js
@@ -9,14 +9,41 @@ module.exports = {
   create: {
     body: Joi.object().keys({
       id_hoja_atencion: Joi.number().required().label("id hoja de atención"),
-      diagnosticos: Joi.array().min(1).items(listaDiagnostico).required().label("lista de diagnosticos"),
-      ficha_familiar: Joi.string().label("id hoja de atención"),
-      ficha_familiar: Joi.string().required().label("id hoja de atención"),
-
-
+      diagnosticos: Joi.array()
+        .min(1)
+        .items(listaDiagnostico)
+        .required()
+        .label("lista de diagnosticos"),
+      ficha_familiar: Joi.string().label("ficha familiar"),
+      cef: Joi.string(),
+      abd: Joi.string(),
+      observacion: Joi.string(),
+      ubigeo: Joi.string().required().label("ubigeo"),
+      peso: Joi.number(),
+      id_financiador: Joi.number(),
+      estado_gestante: Joi.string(),
+      id_centro_poblado: Joi.string(),
+      condicion_establec: Joi.string()
+        .max(2)
+        .required()
+        .label("Condición establecimiento"),
+      condicion_servicio: Joi.string()
+        .max(2)
+        .required()
+        .label("Condición servicio"),
+      fum: Joi.date(),
+      fecha_atencion: Joi.date(),
+      talla: Joi.string(),
+      id_paciente: Joi.number(),
+      fecha_hb: Joi.date(),
+      condicion: Joi.boolean(),
+      num_historia_clinica: Joi.string()
+        .required()
+        .label("Número de historia clinica"),
+      hemoglobina: Joi.number(),
     }),
   },
- /*  edit: {
+  /*  edit: {
     body: Joi.object().keys({
       nombre_turno: Joi.string().required().label("Nombre de turno"),
       id_turno: Joi.number().required().label("Ingrese un id de turno válido"),
