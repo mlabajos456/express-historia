892da771610e0acb44aa74fbf5e91877d07df3f1
Miguel Angel Tantarico Vela <35571294+tantarico@users.noreply.github.com>
2022-09-06 15:28:34 -0500

    USUARIO

--- routes/usuario.route.js
+++ routes/usuario.route.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-/* const validateMiddleware = require("../middlewares/validate.middleware");
-const EmployeeValidationRules = require("../validation-rules/usuario.rule"); */
 const controller = require("../controllers/usuario.controller");
 
 require("express-async-errors");
