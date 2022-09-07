6b1a163e4d9a26c5893b8f3218f96f57d9179680
mlabajos456 <tigerleep26@gmail.com>
2022-09-07 08:14:07 -0500

    okale
    ok

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use("/" + appVersion + "/support/", require("./routes/supports.route"));
 app.use("/" + appVersion + "/", require("./routes/hoja-atencion.route"));
 app.use("/" + appVersion + "/", require("./routes/atencion.route"));
 app.use("/" + appVersion + "/", require("./routes/detalle-diagnostico.route"));
+app.use("/" + appVersion + "/", require("./routes/usuario.route"));
 
 /* app.use("/" + appVersion + "/", jwt, require("./routes/main.route"));
 app.use("/" + appVersion + "/support/", jwt, require("./routes/supports.route")); */
