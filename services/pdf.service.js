c947df86bbe11ea742deef3156124d0cdf82fb11
mlabajos456 <tigerleep26@gmail.com>
2022-09-26 15:43:10 -0500

    ioji
    oki

--- services/pdf.service.js
+++ services/pdf.service.js
@@ -38,43 +38,43 @@ function printHeadPDF (doc, head) {
     }
 
 }
-function printBodyPDF (doc,atencion, diag, position ) {
+function printBodyPDF (doc,atencion, diag, position, tachar ) {
     switch (position) {
     case 1:
-        paintedPosition(doc,atencion,diag,0)
+        paintedPosition(doc,atencion,diag,0,tachar)
         break;
     case 2:
-        paintedPosition(doc,atencion,diag,50)
+        paintedPosition(doc,atencion,diag,50,tachar)
         break;
     case 3:
-        paintedPosition(doc,atencion,diag,99)
+        paintedPosition(doc,atencion,diag,99,tachar)
         break;
     case 4:
-        paintedPosition(doc,atencion,diag,148)
+        paintedPosition(doc,atencion,diag,148,tachar)
         break;
     case 5: 
-        paintedPosition(doc,atencion,diag,198)
+        paintedPosition(doc,atencion,diag,198,tachar)
         break;
     case 6:
-        paintedPosition(doc,atencion,diag,248)
+        paintedPosition(doc,atencion,diag,248,tachar)
         break;
     case 7:
-        paintedPosition(doc,atencion,diag,296.5)
+        paintedPosition(doc,atencion,diag,296.5,tachar)
         break;
     case 8:
-        paintedPosition(doc,atencion,diag,346)
+        paintedPosition(doc,atencion,diag,346,tachar)
         break;
     case 9:
-        paintedPosition(doc,atencion,diag,395.7)
+        paintedPosition(doc,atencion,diag,395.7,tachar)
         break;
     case 10:
-        paintedPosition(doc,atencion,diag,445)
+        paintedPosition(doc,atencion,diag,445,tachar)
         break;
     case 11:
-        paintedPosition(doc,atencion,diag,495)
+        paintedPosition(doc,atencion,diag,495,tachar)
         break;
     case 12:
-        paintedPosition(doc,atencion,diag,545)
+        paintedPosition(doc,atencion,diag,545,tachar)
         break;
 
     default:
@@ -83,146 +83,148 @@ function printBodyPDF (doc,atencion, diag, position ) {
 }
 
 
-function paintedPosition (doc , lista, diag, position){
+function paintedPosition (doc , lista, diag, position,tachar){
     let paciente = lista.paciente.paciente
     let atencion = lista.paciente
-    let diaAtencion = atencion.fecha_atencion.split("-")
-    doc.fontSize(12).text(diaAtencion[0], 39, 198 + position) /* DÍA */
-    //doc.fontSize(6).text(paciente.documento, 62, 185.62 + position) /* DNI */
-    doc.fontSize(6).text(paciente.documento, 59, 185.62 + position,
-        {
-            width: 50,
-        }) /* DNI */
-    doc.fontSize(6).text(atencion.num_historia_clinica, 59, 199 + position) /* DÍA */
-    var gestante = ""
-    if(atencion.estado_gestante =="1"){
-        gestante = "GESTANTE"
-    } 
-    if(atencion.estado_gestante =="2"){
-        gestante = "PUERPERA"
-    } 
+    let diaAtencion = atencion.fecha_atencion.split("-")    
     
-    doc.fontSize(6).text(gestante, 59, 212 + position) /*nombre y Apellidos */
-    doc.fontSize(6).text(paciente.nombres, 126, 174 + position) /* apellido */
-    doc.fontSize(6).text(atencion.id_financiador    , 120, 189 + position) /* financiador con space */
-  
-   
-        
-    //doc.fontSize(6).text("Sanidad", 111, 185 + position) /* financiador con space */
-    //   doc.fontSize(6).text("Naval", 111, 191 + position) /* financiador con space */
-    if(paciente.etnia){
-        var yEtnia= 204
-        if(paciente.etnia.descripcion.length <=7 ){
-            yEtnia = 209
-        }
-        doc.fontSize(4.5).text(paciente.etnia.descripcion, 112, yEtnia + position,{
-            width:22
-        }) /* etnia  */
-    }
-   
-    var yDistrito = 186
-    if(paciente.procedencia.distrito.length < 17){
-        yDistrito = 189
-    }
-    //doc.fontSize(6).text(paciente.procedencia.distrito, 136, 189 + position) /* Distrito de procedencia */
-    doc.fontSize(6).text(paciente.procedencia.distrito, 135, yDistrito + position,
-        {
-            width:60
-        }) /* Distrito de procedencia */
-
-
-    /* centro poblado de procedencia */
-    if(atencion.cp_procedencia){
-        var totalEspacios = atencion.cp_procedencia.descripcion.split(" ")
-        var yProcedencia = 208
-        if(totalEspacios.length ==3){
-            yProcedencia = 205
-        }
-        if(totalEspacios.length ==2){
-            yProcedencia = 205
-        }
-        doc.fontSize(6).text(atencion.cp_procedencia.descripcion, 136, yProcedencia + position,{
-            width:60
-        }) 
-    }
-    let f_anio = atencion.edad_anio;
-    let f_mes = atencion.edad_mes;
-    let f_dia = atencion.edad_dias
-    var edad = "0";
-    if(f_anio !="0"){
-        edad = f_anio
-        doc.moveTo(209, 194 + position).lineTo(220, 182 + position).stroke() /* AÑO */
-    }else if (f_anio =="0" && f_mes =="0"){
-        edad = f_dia
-        doc.moveTo(209, 220 + position).lineTo(220, 208+ position).stroke() /* DÍA */
-    }else if (f_anio =="0" && f_mes != "0"){
-        edad = f_mes
-        doc.moveTo(209, 207 + position).lineTo(220, 195 + position).stroke() /* MES */
-    }
-    
-    doc.fontSize(6).text(edad, 197, 199 + position) /* Edad */
-
-        
-    if(paciente.sexo == "1"){
-        doc.moveTo(221, 201 + position).lineTo(235, 182 + position).stroke() /* masculino */
+    /* if */
+    if(tachar){
+        doc.moveTo(36.54, 220 + position).lineTo(335, 181 + position).stroke()
     }else{
-        doc.moveTo(221, 220 + position).lineTo(235, 201 + position).stroke() /* femenino */     
-    }
+        doc.fontSize(12).text(diaAtencion[0], 39, 198 + position) /* DÍA */
+        doc.fontSize(6).text(paciente.documento, 59, 185.62 + position,
+            {
+                width: 50,
+            }) /* DNI */
+        doc.fontSize(6).text(atencion.num_historia_clinica, 59, 199 + position) /* DÍA */
+        var gestante = ""
+        if(atencion.estado_gestante =="1"){
+            gestante = "GESTANTE"
+        } 
+        if(atencion.estado_gestante =="2"){
+            gestante = "PUERPERA"
+        } 
+        doc.fontSize(6).text(gestante, 59, 212 + position) /*nombre y Apellidos */
+        doc.fontSize(6).text(paciente.nombres, 126, 174 + position) /* apellido */
+        doc.fontSize(6).text(atencion.id_financiador    , 120, 189 + position) /* financiador con space */
+
+        if(paciente.etnia){
+            var yEtnia= 204
+            if(paciente.etnia.descripcion.length <=7 ){
+                yEtnia = 209
+            }
+            doc.fontSize(4.5).text(paciente.etnia.descripcion, 112, yEtnia + position,{
+                width:22
+            }) /* etnia  */
+        }
+   
+        var yDistrito = 186
+        if(paciente.procedencia.distrito.length < 17){
+            yDistrito = 189
+        }
+        //doc.fontSize(6).text(paciente.procedencia.distrito, 136, 189 + position) /* Distrito de procedencia */
+        doc.fontSize(6).text(paciente.procedencia.distrito, 135, yDistrito + position,
+            {
+                width:60
+            }) /* Distrito de procedencia */
+
+
+        /* centro poblado de procedencia */
+        if(atencion.cp_procedencia){
+            var totalEspacios = atencion.cp_procedencia.descripcion.split(" ")
+            var yProcedencia = 208
+            if(totalEspacios.length ==3){
+                yProcedencia = 205
+            }
+            if(totalEspacios.length ==2){
+                yProcedencia = 205
+            }
+            doc.fontSize(6).text(atencion.cp_procedencia.descripcion, 136, yProcedencia + position,{
+                width:60
+            }) 
+        }
+        let f_anio = atencion.edad_anio;
+        let f_mes = atencion.edad_mes;
+        let f_dia = atencion.edad_dias
+        var edad = "0";
+        if(f_anio !="0"){
+            edad = f_anio
+            doc.moveTo(209, 194 + position).lineTo(220, 182 + position).stroke() /* AÑO */
+        }else if (f_anio =="0" && f_mes =="0"){
+            edad = f_dia
+            doc.moveTo(209, 220 + position).lineTo(220, 208+ position).stroke() /* DÍA */
+        }else if (f_anio =="0" && f_mes != "0"){
+            edad = f_mes
+            doc.moveTo(209, 207 + position).lineTo(220, 195 + position).stroke() /* MES */
+        }
+    
+        doc.fontSize(6).text(edad, 197, 199 + position) /* Edad */
+
         
-    doc.fontSize(6).text(atencion.cef, 253, 191 + position) /* PERIMETRO CEFA */
-    doc.fontSize(6).text(atencion.abd, 253, 209 + position)  /* PERIMETRO ABDOMINAL */
+        if(paciente.sexo == "1"){
+            doc.moveTo(221, 201 + position).lineTo(235, 182 + position).stroke() /* masculino */
+        }else{
+            doc.moveTo(221, 220 + position).lineTo(235, 201 + position).stroke() /* femenino */     
+        }
+        
+        doc.fontSize(6).text(atencion.cef, 253, 191 + position) /* PERIMETRO CEFA */
+        doc.fontSize(6).text(atencion.abd, 253, 209 + position)  /* PERIMETRO ABDOMINAL */
 
-    doc.fontSize(6).text(atencion.peso, 289, 185 + position)  /* peso +52 por cada position */
-    doc.fontSize(6).text(atencion.talla, 289, 198 + position)  /* talla + 52 por cada position */
-    if(atencion.hemoglobina === "01-01-2022"){
+        doc.fontSize(6).text(atencion.peso, 289, 185 + position)  /* peso +52 por cada position */
+        doc.fontSize(6).text(atencion.talla, 289, 198 + position)  /* talla + 52 por cada position */
+        if(atencion.hemoglobina === "01-01-2022"){
         /* cortar fecha hemoglobina por dia mes anio */
-        let fecha_hb = atencion.fecha_hb.split("-")
-        doc.fontSize(6).text(fecha_hb[0], 332, 174 + position)  /* DIA + 52 por cada position */
-        doc.fontSize(6).text(fecha_hb[1], 348, 174 + position)  /* MES + 52 por cada position */
-        doc.fontSize(6).text(fecha_hb[2], 365, 174 + position)  /* AÑO + 52 por cada position */
+            let fecha_hb = atencion.fecha_hb.split("-")
+            doc.fontSize(6).text(fecha_hb[0], 332, 174 + position)  /* DIA + 52 por cada position */
+            doc.fontSize(6).text(fecha_hb[1], 348, 174 + position)  /* MES + 52 por cada position */
+            doc.fontSize(6).text(fecha_hb[2], 365, 174 + position)  /* AÑO + 52 por cada position */
 
-    }
-    doc.fontSize(6).text(atencion.hemoglobina, 289, 211 + position)  /* Hemoglobina + 52 por cada position */
+        }
+        doc.fontSize(6).text(atencion.hemoglobina, 289, 211 + position)  /* Hemoglobina + 52 por cada position */
 
     
-    if(atencion.fum === "01-01-2022"){
-        let fecha_fum = atencion.fum.split("-")
-        doc.fontSize(6).text(fecha_fum[0], 532, 174 + position)  /* DIA + 52 por cada position */
-        doc.fontSize(6).text(fecha_fum[1], 549, 174 + position)  /* MES + 52 por cada position */
-        doc.fontSize(6).text(fecha_fum[2], 563, 174 + position)  /* AÑO + 52 por cada position */
-    }
+        if(atencion.fum === "01-01-2022"){
+            let fecha_fum = atencion.fum.split("-")
+            doc.fontSize(6).text(fecha_fum[0], 532, 174 + position)  /* DIA + 52 por cada position */
+            doc.fontSize(6).text(fecha_fum[1], 549, 174 + position)  /* MES + 52 por cada position */
+            doc.fontSize(6).text(fecha_fum[2], 563, 174 + position)  /* AÑO + 52 por cada position */
+        }
       
-    switch (atencion.condicion_establec) {
-    case "N":
-        doc.moveTo(306, 194 + position).lineTo(321, 181 + position).stroke() /* ESTABLECIMIENTO N */  
-        break;
-    case "C":
-        doc.moveTo(306, 207 + position).lineTo(321, 195 + position).stroke() /* ESTABLECIMIENTO C */ 
-        break;
-    case "R":
-        doc.moveTo(306, 220 + position).lineTo(321, 208 + position).stroke() /* ESTABLECIMIENTO R */ 
-        break;
+        switch (atencion.condicion_establec) {
+        case "N":
+            doc.moveTo(306, 194 + position).lineTo(321, 181 + position).stroke() /* ESTABLECIMIENTO N */  
+            break;
+        case "C":
+            doc.moveTo(306, 207 + position).lineTo(321, 195 + position).stroke() /* ESTABLECIMIENTO C */ 
+            break;
+        case "R":
+            doc.moveTo(306, 220 + position).lineTo(321, 208 + position).stroke() /* ESTABLECIMIENTO R */ 
+            break;
     
-    default:
-        break;
-    }
+        default:
+            break;
+        }
                       
      
-    switch (atencion.condicion_servicio) {
-    case "N":
-        doc.moveTo(321, 194 + position).lineTo(335, 181 + position).stroke() /* ESTABLECIMIENTO N */    
-        break;
-    case "C":
-        doc.moveTo(321, 207 + position).lineTo(335, 195 + position).stroke() /* ESTABLECIMIENTO C */  
-        break;
-    case "R":
-        doc.moveTo(321, 220 + position).lineTo(335, 208 + position).stroke() /* ESTABLECIMIENTO R */  
-        break;
+        switch (atencion.condicion_servicio) {
+        case "N":
+            doc.moveTo(321, 194 + position).lineTo(335, 181 + position).stroke() /* ESTABLECIMIENTO N */    
+            break;
+        case "C":
+            doc.moveTo(321, 207 + position).lineTo(335, 195 + position).stroke() /* ESTABLECIMIENTO C */  
+            break;
+        case "R":
+            doc.moveTo(321, 220 + position).lineTo(335, 208 + position).stroke() /* ESTABLECIMIENTO R */  
+            break;
         
-    default:
-        break;
+        default:
+            break;
+        }
     }
    
+    
+    /* if close */
 
     /* console.log(diag[0].cie.descripcion) */
    
@@ -230,7 +232,7 @@ function paintedPosition (doc , lista, diag, position){
     var fontSizeCieFirst = 3
     if(diag[0].cie.descripcion.length < 45){
         yCieFirst = 185
-        fontSizeCieFirst  =6
+        fontSizeCieFirst  =3
     }
     doc.fontSize(fontSizeCieFirst).text(diag[0].cie.descripcion, 341, yCieFirst + position,{
         width: 120,
@@ -274,7 +276,7 @@ function paintedPosition (doc , lista, diag, position){
         var fontSizeCieSecond = 3
         if(diag[1].cie.descripcion.length < 45){
             yCieSecond = 198
-            fontSizeCieSecond = 6
+            fontSizeCieSecond = 3
         }
         doc.fontSize(fontSizeCieSecond).text(diag[1].cie.descripcion, 341, yCieSecond + position,{
             width: 120,
@@ -321,7 +323,7 @@ function paintedPosition (doc , lista, diag, position){
         var fontSizeCieThird = 3
         if(diag[2].cie.descripcion.length < 45){
             yCieThird = 211.5
-            fontSizeCieThird = 6
+            fontSizeCieThird = 3
         }
         doc.fontSize(fontSizeCieThird).text(diag[2].cie.descripcion, 341, yCieThird + position,{
             width: 120,
