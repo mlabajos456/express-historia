4d31b1adf1f4c0d5433ed21d5696c40e4e142353
mlabajos456 <tigerleep26@gmail.com>
2022-09-30 11:21:38 -0500

    validator of duplicity in post atencion prenatal
    it's okay

--- controllers/simys/atencion_prenatal_hoja.controller.js
+++ controllers/simys/atencion_prenatal_hoja.controller.js
@@ -17,7 +17,6 @@ class AtencionPrenatalController {
         let id_gestante = req.query.id_gestante;
         let id_num = req.query.id_num; 
         try {
-           
             atencionPrenatalService.getOneAtencionPrenatal(id_gestante,id_num).then((val) => {
                 response.sendData(res, val, "success");
             }).catch((err) => {
@@ -52,35 +51,48 @@ class AtencionPrenatalController {
         let newAtencion = req.body.atencion;
         let newHojaAtencion = req.body.hoja_atencion;
         let gestante = await gestanteService.getOneGestante(req.body.atencion_prenatal.id_gestante)
-        let atencionPrenatal = req.body.atencion_prenatal;
+        let atencionPrenatal = req.body.atencion_prenatal;       
 
         try {
-            var newHojaCreated =await hojaAtencionService.postHojaAtencion(t,newHojaAtencion);
-            var atencion = await db["his_atencion"].build(newAtencion);
-            atencion.id_hoja_atencion = newHojaCreated.id_hoja_atencion;
-            atencion.fecha_atencion = Date.now();            
-            atencion.id_paciente = gestante.paciente.id
-            var newAtencionCreated = await atencion.save({ transaction: t });
-            
-            atencionPrenatal.id_atencion = newAtencionCreated.id_atencion;
-            await atencionPrenatalService.postAtencionPrenatal(t,atencionPrenatal)
-
-
-            for (const detail of newAtencion.diagnosticos) {
-                detail.id_cie = detail.id_cie.id
-                var detailDiag = await db["his_detalle_diagnostico"].build(detail);
-                detailDiag.id_atencion = newAtencionCreated.id_atencion;
-                
-                var newDetail = await detailDiag.save({ transaction: t });
-                for (const lab of detail.valor_lab){
-                    lab.id_detalle = newDetail.id_detalle
-                    var newLab = await db["his_lab"].build(lab);
-                    await newLab.save({ transaction: t });
+            let detalleAtencionPrenatal = await atencionPrenatalService.getOneAtencionPrenatal(atencionPrenatal.id_gestante, atencionPrenatal.id_num)
+            if(!detalleAtencionPrenatal){
+                /* se crea la hoja */
+                var newHojaCreated =await hojaAtencionService.postHojaAtencion(t,newHojaAtencion);
+                /* se modela la atencion para posteriormente guardar */
+                var atencion = await db["his_atencion"].build(newAtencion);
+                atencion.id_hoja_atencion = newHojaCreated.id_hoja_atencion;
+                atencion.fecha_atencion = Date.now();            
+                atencion.id_paciente = gestante.paciente.id
+                var newAtencionCreated = await atencion.save({ transaction: t });            
+                /* se registra el detalle de la atencion prenatal */
+                atencionPrenatal.id_atencion = newAtencionCreated.id_atencion;
+                await atencionPrenatalService.postAtencionPrenatal(t,atencionPrenatal)
+                /* se registra los diagnoticos de la atencion */
+                for (const detail of newAtencion.diagnosticos) {
+                    detail.id_cie = detail.id_cie.id
+                    var detailDiag = await db["his_detalle_diagnostico"].build(detail);
+                    detailDiag.id_atencion = newAtencionCreated.id_atencion;
+                    /* se registra los valores lab de los diagnosticos */
+                    var newDetail = await detailDiag.save({ transaction: t });
+                    for (const lab of detail.valor_lab){
+                        lab.id_detalle = newDetail.id_detalle
+                        var newLab = await db["his_lab"].build(lab);
+                        await newLab.save({ transaction: t });
+                    }
                 }
+                await t.commit();
+                response.sendCreated(res, req.body);
+            }else{
+                req.body.atencion_prenatal.id_atencion = detalleAtencionPrenatal.id_atencion
+                req.body.hoja_atencion.id_hoja_atencion = detalleAtencionPrenatal.his_atencion.his_hoja_atencion.id_hoja_atencion
+                req.body.atencion.id_atencion = detalleAtencionPrenatal.id_atencion
+                req.body.atencion.id_hoja_atencion = detalleAtencionPrenatal.his_atencion.his_hoja_atencion.id_hoja_atencion
+                new AtencionPrenatalController().putAtencionPrenatal(req, res) //
             }
             
-            await t.commit();
-            response.sendCreated(res, newAtencionCreated);
+            
+         
+          
         } catch (error) {
             await t.rollback();
             response.sendBadRequest(res, error.message);
@@ -117,7 +129,6 @@ class AtencionPrenatalController {
                     await newLab.save({ transaction: t });
                 }         
             }
-
             t.commit();
          
             response.sendData(res, atencion, "Se ha actualizado correctamente");
